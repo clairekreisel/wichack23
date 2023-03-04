b725c1ae46e004a152fe7271fef60c02e797b426
Avery Carle <ajc4018@rit.edu>
2023-03-04 17:38:59 -0500

    tried to iterate over all the pages for a location , but it was being weird, so I commented it out

--- webscraping/find_text.py
+++ webscraping/find_text.py
@@ -2,16 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 import subprocess
 
+
+
+
 def find_texts(location, page=0) :
     if page > 0:
         page = "?page=" + str(page);
     else:
         page = ""
     url = 'https://legiscan.com/' + location + '/legislation' + page
-    print(url)
     r = requests.get(url)
     
     soup = BeautifulSoup(r.content, "html.parser")
+    return __get_links(soup)
+
+def __get_links(soup):
     a_elements = soup.find_all('a', class_='gaits-bill-detail')
     links = [];
     for a in a_elements:
@@ -19,8 +24,27 @@ def find_texts(location, page=0) :
 
     return links
 
+# def find_all_texts(location) :
+#     url = 'https://legiscan.com/' + location + '/legislation'
+#     r = requests.get(url);
+#     soup = BeautifulSoup(r.content, "html.parser")
+#     links = __get_links(soup);
+#     page = 1
+#     page_options = soup.find_all('li',  class_='pager-item')
+#     last_page_option = page_options[len(page_options) - 1]
+#     while (last_page_option.content != 47) :
+#         print(len(links))
+#         links = links + find_texts(location, page)
+#         page_options = soup.find_all('li',  class_='pager-item')
+#         last_page_option = page_options[len(page_options) - 1]
+#         page += 1
+
+#     return links
+
+
 def main() :
     print(find_texts("US"))
     print(find_texts("NH", 2))
+    # print(find_all_texts("US"))
 
 if __name__ == "__main__" : main()
