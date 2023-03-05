38487946d5dff382248b6ff80afa870b29c6ff0c
Avery Carle <ajc4018@rit.edu>
2023-03-04 19:44:16 -0500

    documentation

--- webscraping/find_text.py
+++ webscraping/find_text.py
@@ -1,10 +1,28 @@
+"""
+This file is for finding the bill texts from legiscan.
+For Claire, Avery, and Rosaline's WicHacks 2023 Project
+
+this file written primarily by Avery Carle
+
+copyleft under GNU GPL 3.0 License
+"""
+
 import requests
 from bs4 import BeautifulSoup
-import subprocess
 
 
 
+"""
+Scans the legiscan page for a location and returns a list of links to texts
 
+Parameters:
+    location: String of the two letters of the state to scan (US for federal and DC for Washington D.C.)
+    page: 0-indexed value of which page of the bills to scan (0 by default)
+
+Returns: a list of the links to bill texts from that page
+
+note: some pages are blank for some reason, not sure why
+"""
 def find_texts(location, page=0) :
     if page > 0:
         page = "?page=" + str(page);
@@ -16,6 +34,14 @@ def find_texts(location, page=0) :
     soup = BeautifulSoup(r.content, "html.parser")
     return __get_links(soup)
 
+"""
+Helper function for parsing site into the list of links
+
+Parameters:
+    soup: a BeautifulSoup object for a legiscan legislation page
+
+Returns: a list of the links to bill texts from that page
+"""
 def __get_links(soup):
     a_elements = soup.find_all('a', class_='gaits-bill-detail')
     links = [];
